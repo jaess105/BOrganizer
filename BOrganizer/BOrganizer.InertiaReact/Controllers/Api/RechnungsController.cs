1956cce93a570bd980d9e56944639423b9b4707a
jaess105 <jaess105@uni-duesseldorf.de>
2025-07-13 10:13:40 +0200

    Add a search endpoint to the RechnungController

--- BOrganizer/BOrganizer.InertiaReact/Controllers/Api/RechnungsController.cs
+++ BOrganizer/BOrganizer.InertiaReact/Controllers/Api/RechnungsController.cs
@@ -1,15 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Rechnungen;
+using Rechnungen.Model.Invoices;
 using Rechnungen.Services.Invoices;
 
 namespace BOrganizer.InertiaRact.Controllers.Api;
 
-[Route("Api/Rechnung/Creation/Pdf")]
+[Route("Api/Rechnung")]
 public class RechnungsController(
     IRechnungsService rechnungsService,
     IRechnungsCreator rechnungsCreator) : Controller
 {
-    [HttpGet("{invoiceId:long}")]
+    [HttpGet("Creation/Pdf/{invoiceId:long}")]
     public async Task<IActionResult> OnPostDownload(
         long? invoiceId,
         bool download = false)
@@ -27,5 +28,20 @@ public class RechnungsController(
         string disposition = download ? "attachment" : "inline";
         Response.Headers.Append("Content-Disposition", $"{disposition}; filename={pdfName}");
         return File(invoicePdfStream, "application/pdf");
+    }
+
+
+    [HttpGet("Search")]
+    public async Task<IActionResult> Search(string query)
+    {
+        if (string.IsNullOrWhiteSpace(query)) { return Ok(new List<object>()); }
+
+        IEnumerable<RechnungsNummer> rechnungsNummern = await rechnungsService.SearchRechnungsNummernAsync(query);
+
+        return Ok(rechnungsNummern.Select(rn => new
+        {
+            rn.Id,
+            label = rn.ToString(),
+        }));
     }
 }
